8267be7b09bad6be4cb668b2823f9655a11695d1
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-12 23:13:41 +0530

    Getting Started with  React Project creation using Vite

---
+++ React Learning By Sanket Sir PW/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+function Title(){
+  return (
+    <>
+      <h1>Hello </h1>
+      <h1>This is Title  </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero quis quibusdam, placeat suscipit, nisi consectetur, rem corporis a repellendus architecto laboriosam expedita facilis beatae dolorum alias ipsum sapiente modi quod?</p>
+    </>
+  )
+}
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
