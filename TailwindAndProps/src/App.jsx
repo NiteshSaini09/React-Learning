a40d0e328d21384ebc804bf8a758b7ab81f715bc
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-14 23:34:14 +0530

    tailwind

---
+++ TailwindAndProps/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { HeaderNavigationDualTierDemo } from "./Nav.jsx";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <HeaderNavigationDualTierDemo />
+      <p>Hello</p>
+    </>
+  );
+}
+
+export default App;
