677425735e2827caea8370df1edebd82fac0a8a6
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-16 13:14:38 +0530

    feat: add HeroSection9 component and integrate it into the main App layout

--- backgroundColorChanger/src/App.jsx
+++ backgroundColorChanger/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import ColorTab from "./ColorTab.jsx";
 import HeroSaaS2 from "./Hero.jsx";
+import HeroSection9 from "./LoginForm.jsx";
 function App() {
   const [color,setColor]=useState("white")
   return (
     <div className="flex-1 w-screen" style={{backgroundColor:color}}>
         <HeroSaaS2/>
-      <div className="mt-150">
+      <div className="flex flex-col gap-20 mb-20">
+        <HeroSection9/>
         <ColorTab setColor={setColor}/>
       </div>
     </div>
