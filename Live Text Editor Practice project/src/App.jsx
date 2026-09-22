9508351b6a52b0e58cfd99cc8c147b1dbb876f87
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-22 23:50:58 +0530

    feat: add button to convert text to uppercase in Live Text Editor

--- Live Text Editor Practice project/src/App.jsx
+++ Live Text Editor Practice project/src/App.jsx
@@ -11,7 +11,9 @@ function App() {
     id="text" 
     className='outline rounded-lg px-20 mt-20 text-green-500'
     onChange={(e)=>setText(e.target.value)}
+    value={text}
     ></textarea>
+    <button className='border mt-2 max-w-30 rounded-lg px-5 py-2 bg-blue-900 cursor-pointer' onClick={(e)=>setText(text.toUpperCase())}>UpperCase</button>
     <h5>Your Text : {text}</h5>
     <h5>Your Text Length:{text.length}</h5>
     <h5>Words: {words}</h5>
