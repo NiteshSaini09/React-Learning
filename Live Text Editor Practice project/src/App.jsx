1f2dfd48b129d6a71791d3dad8abee823c1a7438
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-23 23:23:12 +0530

    Add initial setup for Currency Convertor with React, Vite, and Tailwind CSS
    - Created main entry point (main.jsx) for React application.
    - Added Tailwind CSS import in index.css.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.
    - Included SVG assets for React and Vite logos

--- Live Text Editor Practice project/src/App.jsx
+++ Live Text Editor Practice project/src/App.jsx
@@ -2,8 +2,8 @@ import { useState} from 'react'
 import './App.css'
 
 function App() {
-  const [text, setText] = useState("")
-  let words=text.split(" ").length
+  let [text, setText] = useState("")
+  let words=text.trim().split(" ").length-1
   return (
   <>
     <textarea 
@@ -15,9 +15,11 @@ function App() {
     ></textarea>
     <button className='border mt-2 max-w-30 rounded-lg px-5 py-2 bg-blue-900 cursor-pointer' onClick={(e)=>setText(text.toUpperCase())}>UpperCase</button>
     <button className='border mt-2 max-w-30 rounded-lg px-5 py-2 bg-blue-900 cursor-pointer' onClick={(e)=>setText(text.toLowerCase())}>LowerCase</button>
+    <button className='border mt-2 max-w-30 rounded-lg px-5 py-2 bg-blue-900 cursor-pointer' onClick={()=>setText("")}>Clear</button>
     <h5>Your Text : {text}</h5>
-    <h5>Your Text Length:{text.length}</h5>
+    <h5>Your Text Length:{text.trim().length}</h5>
     <h5>Words: {words}</h5>
+    <p>Preview : {text}</p>
 
   </>
   )
