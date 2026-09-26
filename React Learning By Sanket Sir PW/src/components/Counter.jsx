ed4efc3ac17823bbbebbaee15181168ed0f9c05a
Nitesh Saini <nitesh.tech.dev@gmail.com>
2026-09-26 23:33:42 +0530

    refactor: enhance Counter component with separate increment and decrement functions; add odd/even display

--- React Learning By Sanket Sir PW/src/components/Counter.jsx
+++ React Learning By Sanket Sir PW/src/components/Counter.jsx
@@ -1,13 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function Counter(){
-    let [count,setCount]=useState(0)
-    return(
-        <>
-            <h1>{count}</h1>
-            <button className="border px-3 py-1 ml-2 rounded bg-green-500 font-bold hover:bg-green-700 " onClick={()=>setCount(count+1)}>Count ++</button>
-            <button className="border px-3 py-1 ml-2 rounded bg-red-500 font-bold hover:bg-red-700 " onClick={()=>setCount(count-1)}>Count --</button>
-        </>
-    )
+function Counter() {
+  let [count, setCount] = useState(0);
+  let inc = () => {
+    setCount(count + 1);
+  };
+  let dec = () => {
+    if (count >= 1) setCount(count - 1);
+  };
+  return (
+    <>
+      <h1>{count}</h1>
+      <h3>
+        {count} is an {count % 2 == 0 ? "Even" : "Odd"}
+      </h3>
+      <button
+        className="border px-3 py-1 ml-2 rounded bg-green-500 font-bold hover:bg-green-700 "
+        onClick={inc}
+      >
+        Count ++
+      </button>
+      <button
+        className="border px-3 py-1 ml-2 rounded bg-red-500 font-bold hover:bg-red-700 "
+        onClick={dec}
+      >
+        Count --
+      </button>
+    </>
+  );
 }
-export default Counter
+export default Counter;
